dc9740879c73be0fd16b499b044ad80c34dafd3c
arongeo <me@arongeo.com>
2023-04-18 01:29:48 +0200

    Successfully decoding at 25 fps

--- src/main.rs
+++ src/main.rs
@@ -2,85 +2,61 @@ use std::{net::UdpSocket, time::Duration, io::stdin, path::Path};
 use openh264::decoder::Decoder;
 use image;
 
-const NAL_MIN_0_COUNT: usize = 2;
+fn check_for_valid_packet(data_stream: &[u8]) -> Option<(usize, usize)> {
+    let mut zero_seq_len = 0;
+    let mut first = None;
 
-/// Given a stream, finds the index of the nth NAL start.
-fn nth_nal_index(stream: &[u8], nth: usize) -> Option<usize> {
-    let mut count_0 = 0;
-    let mut n = 0;
-
-    for (i, byte) in stream.iter().enumerate() {
-        match byte {
-            0 => count_0 += 1,
-            1 if count_0 >= NAL_MIN_0_COUNT => {
-                if n == nth {
-                    return Some(i - NAL_MIN_0_COUNT);
-                } else {
-                    count_0 = 0;
-                    n += 1;
-                }
+    for i in 0..data_stream.len() {
+        match data_stream[i] {
+            0 => {
+                zero_seq_len += 1;
+            },
+            1 => {
+                if zero_seq_len >= 2 {
+                    if first != None {
+                        return Some((first.unwrap(), i - 2));
+                    } else {
+                        first = Some(i - 2);
+                    }
+                } 
             }
-            _ => count_0 = 0,
+            _ => {
+                zero_seq_len = 0;
+            }
         }
     }
+
 
     None
 }
 
-pub fn nal_units(mut stream: &[u8]) -> impl Iterator<Item = &[u8]> {
-    std::iter::from_fn(move || {
-        let first = nth_nal_index(stream, 0);
-        let next = nth_nal_index(stream, 1);
 
-        match (first, next) {
-            (Some(f), Some(n)) => {
-                let rval = &stream[f..n];
-                stream = &stream[n..];
-                Some(rval)
-            }
-            (Some(f), None) => {
-                let rval = &stream[f..];
-                stream = &stream[f + NAL_MIN_0_COUNT..];
-                Some(rval)
-            }
-            _ => None,
-        }
-    })
-}
-
-fn h264_decode(frame_data: &[u8]) -> Result<i32, ()> {
-    let mut decoder = Decoder::new().unwrap();
-
+fn h264_decode(frame_data: &[u8], decoder: &mut Decoder) -> Result<Vec<u8>, ()> {
     let mut buffer = vec![0; 2764800];
     let mut img_rendered = 0;
-    for data in nal_units(frame_data) {
-        let yuv = match decoder.decode(data) {
-            Ok(o) => {
-                match o {
-                    Some(y) => {
-                        println!("Decoded at {}", frame_data.len());
-                        img_rendered += 1;
-                        y
-                    },
-                    None => {
-                        //return Err(());
-                        continue;
-                    },
-                }
-            },
-            Err(_) => {
-                //return Err(());
-                continue;
-            },
-        };
+    let yuv = match decoder.decode(frame_data) {
+        Ok(o) => {
+            match o {
+                Some(y) => {
+                    img_rendered += 1;
+                    y
+                },
+                None => {
+                    return Err(());
+                    //continue;
+                },
+            }
+        },
+        Err(_) => {
+            return Err(());
+            //continue;
+        },
+    };
 
+    let dims = yuv.dimension_rgb();
 
-        let dims = yuv.dimension_rgb();
+    yuv.write_rgba8(&mut buffer);
 
-        yuv.write_rgba8(&mut buffer);
-
-
-    }
     /*
     print!("COM: [");
     for i in 0..data.len() {
@@ -98,7 +74,7 @@ fn h264_decode(frame_data: &[u8]) -> Result<i32, ()> {
     */
         
     if 0 < img_rendered {
-        Ok(img_rendered as i32)
+        Ok(buffer)
     } else {
         Err(())
     }
@@ -128,11 +104,10 @@ fn main() {
         };
     
         let mut frame_packet = vec![];
-        let mut ptr = 0;
-        let mut a = 0;
-        let mut lost = 0;
 
+        let mut decoder = Decoder::new().unwrap();
         let mut now = std::time::Instant::now();
+        let mut a = 0;
 
         loop {
             let mut video_buffer = [0; 1460];
@@ -154,48 +129,28 @@ fn main() {
             */
 
             frame_packet.extend_from_slice(&video_buffer[..msg_len]);
-            ptr += msg_len;
 
-            /*
-            let mut first = nth_nal_index(&frame_packet, 0);
-            let mut second = nth_nal_index(&frame_packet, 1);
-            if (first != None) & (second != None) {
-                let first_i = first.unwrap();
-                let second_i = second.unwrap();
-
-                match h264_decode(&frame_packet[first_i..second_i]) {
-                    Ok(_) => {
-                        let mut frame_packet_replace = [0; 20000];
-                        frame_packet_replace[0..(ptr-second_i)].clone_from_slice(&frame_packet[second_i..ptr]);
-                        frame_packet = frame_packet_replace;
-                        ptr = ptr - second_i;
-
-                        a += 1;
-
-                        if now.elapsed().as_secs_f64() >= 1.0 {
-                            now = std::time::Instant::now();
-                            println!("Got: {}", a);
-                            a = 0;
+            match check_for_valid_packet(&frame_packet) {
+                Some(frame_borders) => {
+                    match h264_decode(&frame_packet[(frame_borders.0)..(frame_borders.1)], &mut decoder) {
+                        Ok(_) => {
+                            frame_packet = frame_packet[(frame_borders.1)..(frame_packet.len())].to_vec();
+                            
+                            a += 1;
+                            if now.elapsed().as_secs_f64() >= 1.0 {
+                                now = std::time::Instant::now();
+                                println!("Got: {}", a);
+                                a = 0;
+                            }
+                        },
+                        Err(_) => {
+                            frame_packet = frame_packet[(frame_borders.1)..(frame_packet.len())].to_vec();
                         }
-                    },
-                    Err(_) => {
-                        let mut frame_packet_replace = [0; 20000];
-                        frame_packet_replace[0..(ptr-second_i)].clone_from_slice(&frame_packet[second_i..ptr]);
-                        frame_packet = frame_packet_replace;
-                        ptr = ptr - second_i;
-
-                    }
-                };
-            }
-            */
-
-
-            match h264_decode(&frame_packet) {
-                Ok(nof) => {
-                    println!("Frames decoded: {}", nof);
+                    };
                 },
-                Err(_) => continue,
-            };
+                None => continue,
+            }
+
         }
     });
 
