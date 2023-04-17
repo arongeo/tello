745da10b3a829e879896f56d47975a245107f335
arongeo <me@arongeo.com>
2023-04-18 01:48:10 +0200

    FFMPEG tests

--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,11 @@
 use std::{net::UdpSocket, time::Duration, io::stdin, path::Path};
-use openh264::decoder::Decoder;
+use ffmpeg::codec::Context;
+use ffmpeg::codec::traits::Decoder;
+use ffmpeg_next as ffmpeg;
+use ffmpeg::util::frame::Video as VFrame;
+use ffmpeg::software::scaling::{context::Context as ScalingContext, flag::Flags};
+use ffmpeg::{Packet, Frame};
 use image;
-
-const NAL_MIN_0_COUNT: usize = 2;
-
-/// Given a stream, finds the index of the nth NAL start.
-fn nth_nal_index(stream: &[u8], nth: usize) -> Option<usize> {
-    let mut count_0 = 0;
-    let mut n = 0;
-
-    for (i, byte) in stream.iter().enumerate() {
-        match byte {
-            0 => count_0 += 1,
-            1 if count_0 >= NAL_MIN_0_COUNT => {
-                if n == nth {
-                    return Some(i - NAL_MIN_0_COUNT);
-                } else {
-                    count_0 = 0;
-                    n += 1;
-                }
-            }
-            _ => count_0 = 0,
-        }
-    }
-
-    None
-}
-
-pub fn nal_units(mut stream: &[u8]) -> impl Iterator<Item = &[u8]> {
-    std::iter::from_fn(move || {
-        let first = nth_nal_index(stream, 0);
-        let next = nth_nal_index(stream, 1);
-
-        match (first, next) {
-            (Some(f), Some(n)) => {
-                let rval = &stream[f..n];
-                stream = &stream[n..];
-                Some(rval)
-            }
-            (Some(f), None) => {
-                let rval = &stream[f..];
-                stream = &stream[f + NAL_MIN_0_COUNT..];
-                Some(rval)
-            }
-            _ => None,
-        }
-    })
-}
-
-fn h264_decode(frame_data: &[u8]) -> Result<i32, ()> {
-    let mut decoder = Decoder::new().unwrap();
-
-    let mut buffer = vec![0; 2764800];
-    let mut img_rendered = 0;
-    for data in nal_units(frame_data) {
-        let yuv = match decoder.decode(data) {
-            Ok(o) => {
-                match o {
-                    Some(y) => {
-                        println!("Decoded at {}", frame_data.len());
-                        img_rendered += 1;
-                        y
-                    },
-                    None => {
-                        //return Err(());
-                        continue;
-                    },
-                }
-            },
-            Err(_) => {
-                //return Err(());
-                continue;
-            },
-        };
-
-
-        let dims = yuv.dimension_rgb();
-
-        yuv.write_rgba8(&mut buffer);
-
-
-    }
-    /*
-    print!("COM: [");
-    for i in 0..data.len() {
-        print!("{}, ", data[i]);
-    }
-    print!("]\n");
-    */
-
-    /*
-    print!("ME: [");
-    for i in 0..according_to_me.len() {
-        print!("{}, ", according_to_me[i]);
-    }
-    print!("]\n");
-    */
-        
-    if 0 < img_rendered {
-        Ok(img_rendered as i32)
-    } else {
-        Err(())
-    }
-}
 
 fn main() {
     let sock = match UdpSocket::bind("0.0.0.0:62000") {
@@ -122,81 +25,84 @@ fn main() {
 
     let thread_thing = std::thread::spawn(|| {
         // receive video stream from tello on port 11111
-        let mut video_socket = match UdpSocket::bind("0.0.0.0:11111") {
+        let video_socket = match UdpSocket::bind("0.0.0.0:11111") {
             Ok(s) => s,
             Err(e) => panic!("ERROR with creating socket: {}", e),
         };
-    
-        let mut frame_packet = vec![];
-        let mut ptr = 0;
-        let mut a = 0;
-        let mut lost = 0;
+
+        ffmpeg::init().unwrap();
+
+        let codec = ffmpeg::decoder::find(ffmpeg::codec::Id::H264).unwrap();
+
+        let context = Context::new();
+        let decoder = context.decoder();
+        let opened = match decoder.open_as(codec) {
+            Ok(opened) => opened,
+            Err(e) => panic!("Failed to open decoder: {}", e),
+        };
+
+        let mut vdecoder = match opened.video() {
+            Ok(vd) => vd,
+            Err(e) => panic!("Couldn't get video decoder from decoder: {}", e),
+        };
 
         let mut now = std::time::Instant::now();
+        let mut frame_counter = 0;
 
+        let mut frame_buffer = vec![];
         loop {
-            let mut video_buffer = [0; 1460];
-            let msg_len = match video_socket.recv(&mut video_buffer) {
-                Ok(ml) => ml,
+            let mut vbuffer = [0; 1460];
+            
+            let msg_len = match video_socket.recv(&mut vbuffer) {
+                Ok(l) => l,
                 Err(_) => continue,
             };
 
-            /*
-            frame_packet[ptr..(ptr+msg_len)].clone_from_slice(&video_buffer[..msg_len]); //video_buffer[i - ptr];
-            ptr += msg_len;
-            */
+            frame_buffer.extend_from_slice(&vbuffer[..msg_len]);
 
-            /*
-            for i in 0..(msg_len) {
-                frame_packet[i + ptr] = video_buffer[i];
+            if msg_len < 15 {
+                continue;
             }
-            ptr += msg_len;
-            */
 
-            frame_packet.extend_from_slice(&video_buffer[..msg_len]);
-            ptr += msg_len;
+            if (msg_len < 1460) {
+                let packet = Packet::copy(&frame_buffer);
 
-            /*
-            let mut first = nth_nal_index(&frame_packet, 0);
-            let mut second = nth_nal_index(&frame_packet, 1);
-            if (first != None) & (second != None) {
-                let first_i = first.unwrap();
-                let second_i = second.unwrap();
+                let mut frame = unsafe { Frame::empty() };
 
-                match h264_decode(&frame_packet[first_i..second_i]) {
-                    Ok(_) => {
-                        let mut frame_packet_replace = [0; 20000];
-                        frame_packet_replace[0..(ptr-second_i)].clone_from_slice(&frame_packet[second_i..ptr]);
-                        frame_packet = frame_packet_replace;
-                        ptr = ptr - second_i;
+                match vdecoder.send_packet(&packet) {
+                    Ok(_) => {},
+                    Err(e) => println!("Error with sending the packet: {:?}", e),
+                }
 
-                        a += 1;
+                match vdecoder.receive_frame(&mut frame) {
+                    Ok(_) => { 
 
-                        if now.elapsed().as_secs_f64() >= 1.0 {
+                        let mut scaler = ScalingContext::get(
+                            vdecoder.format(),
+                            vdecoder.width(),
+                            vdecoder.height(),
+                            ffmpeg::format::Pixel::RGB24,
+                            vdecoder.width(),
+                            vdecoder.height(),
+                            Flags::BILINEAR,
+                        ).unwrap();
+
+                        frame_counter += 1;
+                        if now.elapsed().as_secs_f64() > 1.0 {
+                            frame_counter = 0;
                             now = std::time::Instant::now();
-                            println!("Got: {}", a);
-                            a = 0;
                         }
+
                     },
-                    Err(_) => {
-                        let mut frame_packet_replace = [0; 20000];
-                        frame_packet_replace[0..(ptr-second_i)].clone_from_slice(&frame_packet[second_i..ptr]);
-                        frame_packet = frame_packet_replace;
-                        ptr = ptr - second_i;
-
-                    }
+                    Err(e) => {
+                        std::thread::sleep_ms(10);
+                    },
                 };
+
+                frame_buffer = vec![];
             }
-            */
-
-
-            match h264_decode(&frame_packet) {
-                Ok(nof) => {
-                    println!("Frames decoded: {}", nof);
-                },
-                Err(_) => continue,
-            };
         }
+
     });
 
     loop {
